c357a96796d9b6469b15c1ff03a48e5b9f138ed9
FranciscoRenmer <39177485+Renmer@users.noreply.github.com>
2022-06-16 10:55:48 -0500

    Creacion del proyecto y primeras implementaciones

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Nombre
+          </th>
+          <th class="text-center">
+            Sinopsis
+          </th>
+          <th class="text-center">
+            Estrellas
+          </th>
+          <th class="text-center">
+            Opciones
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in peliculas"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+  export default {
+    name: 'Home',
+    data(){
+      return{
+        peliculas:[],
+      }
+    },
+    mounted(){
+
+    }
   }
-}
 </script>
